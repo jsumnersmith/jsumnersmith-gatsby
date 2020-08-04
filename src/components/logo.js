f27f8fa185a99ddafd0cbb89d6f763ff74346968
Joel Smith <jsumnersmith@gmail.com>
2020-08-04 08:39:06 -0400

    Removed Gatsby image for flicker issues

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,33 +1,16 @@
 /** @jsx jsx */
-import { useStaticQuery, graphql } from "gatsby"
 import { jsx, useColorMode, Box} from "theme-ui"
-import Img from "gatsby-image"
+import lightLogo from '../images/light-mug.png'
+import darkLogo from '../images/dark-mug.png'
 
 const Image = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      lightLogo: file(relativePath: { eq: "light-mug.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      darkLogo: file(relativePath: { eq: "dark-mug.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-
   const [colorMode] = useColorMode();
+  const image = colorMode === "default" ? lightLogo : darkLogo;
 
-  const image = colorMode === "default" ? data.lightLogo : data.darkLogo;
-
-  return <Box sx={{width: 100, margin: `0 auto`, mb: 3}}><Img fluid={image.childImageSharp.fluid} /></Box>
+  return (
+    <Box sx={{width: 100, margin: `0 auto`, mb: 3}}>
+      <img src={image} alt="fancy mug logo" sx={{width: `100%`}}/>
+    </Box>)
 }
 
 export default Image
