58d05763122a9af5e6215117e2023fe7172d5f9e
Joel Smith <jsumnersmith@gmail.com>
2021-01-29 09:52:54 -0500

    make it not use fs

--- src/functions/sendMail.js
+++ src/functions/sendMail.js
@@ -1,26 +1,29 @@
-const sendgrid = require('@sendgrid/mail');
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'joel@gatsbyjs.com',
-  from: 'jsumnersmith@gmail.com', // Use the email address or domain you verified above
-  subject: 'This is a Gatsby Function',
-  text: 'Gatsby go brrrrrrrrr',
-  html: '<strong>Gatsby go brrrrrrrrr</strong>',
-}
+// const sendgrid = require('@sendgrid/mail');
+// sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+// const msg = {
+//   to: 'joel@gatsbyjs.com',
+//   from: 'jsumnersmith@gmail.com', // Use the email address or domain you verified above
+//   subject: 'This is a Gatsby Function',
+//   text: 'Gatsby go brrrrrrrrr',
+//   html: '<strong>Gatsby go brrrrrrrrr</strong>',
+// }
 
 module.exports = (req, res) => {
-  sendgrid
-  .send(msg)
-  .then(() => {}, error => {
-    console.error(error);
+  // sendgrid
+  // .send(msg)
+  // .then(() => {}, error => {
+  //   console.error(error);
 
-    if (error.response) {
-      return res.status(500).json({
-        error: error.response
-      })
-    }
-    return res.status(201).json({
-      results: "Mail Sent"
-    })
-  });
+  //   if (error.response) {
+  //     return res.status(500).json({
+  //       error: error.response
+  //     })
+  //   }
+  //   return res.status(201).json({
+  //     results: "Mail Sent"
+  //   })
+  // });
+  res.status(200).json({
+    message: "I will send email"
+  })
 }
