5c7618f853ea549e33a77e23d7393837499836cd
Joel Smith <jsumnersmith@gmail.com>
2021-02-05 08:34:05 -0500

    Stashing work.

--- src/functions/sendMail.js
+++ src/functions/sendMail.js
@@ -1,29 +1,29 @@
-// const sendgrid = require('@sendgrid/mail');
-// sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
-// const msg = {
-//   to: 'joel@gatsbyjs.com',
-//   from: 'jsumnersmith@gmail.com', // Use the email address or domain you verified above
-//   subject: 'This is a Gatsby Function',
-//   text: 'Gatsby go brrrrrrrrr',
-//   html: '<strong>Gatsby go brrrrrrrrr</strong>',
-// }
-
-module.exports = (req, res) => {
-  // sendgrid
-  // .send(msg)
-  // .then(() => {}, error => {
-  //   console.error(error);
-
-  //   if (error.response) {
-  //     return res.status(500).json({
-  //       error: error.response
-  //     })
-  //   }
-  //   return res.status(201).json({
-  //     results: "Mail Sent"
-  //   })
-  // });
-  res.status(200).json({
-    message: "I will send email"
-  })
+const sendgrid = require('@sendgrid/mail');
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'joel@gatsbyjs.com',
+  from: 'jsumnersmith@gmail.com', // Use the email address or domain you verified above
+  subject: 'This is a Gatsby Function',
+  text: 'Gatsby go brrrrrrrrr',
+  html: '<strong>Gatsby go brrrrrrrrr</strong>',
 }
+
+// module.exports = (req, res) => {
+  return sendgrid
+  .send(msg)
+  .then(() => {}, error => {
+    console.error(error);
+
+    if (error.response) {
+      return res.status(500).json({
+        error: error.response
+      })
+    }
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+  });
+  
+  // res.status(200).json({
+  //   message: "I will send email"
+  // })
+// }
