e78f5ad3806c08b8aa25908129fb789000c97211
Joel Smith <jsumnersmith@gmail.com>
2021-02-22 15:39:53 -0500

    TEsting.

--- src/functions/sendSMS.js
+++ src/functions/sendSMS.js
@@ -1,12 +1,30 @@
-const account_sid = process.env.GATSBY_TWILIO_ACCOUNT_SID;
-const auth_token = process.env.GATSBY_TWILIO_AUTH_TOKEN;
-const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-module.exports = (req, res) => {
-  client()
+// require('dotenv');
+// const account_sid = process.env.GATSBY_TWILIO_ACCOUNT_SID;
+// const auth_token = process.env.GATSBY_TWILIO_AUTH_TOKEN;
+const client = require('twilio')(process.env.GATSBY_TWILIO_ACCOUNT_SID,  process.env.GATSBY_TWILIO_AUTH_TOKEN);
 
-  client.messages
+module.exports = (req, res) => {
+  const body = req.body;
+  console.log(body);
+  
+  let message = 'I am a Gatsby Function.'
+
+  if (body && body.message) {
+    return client.messages
     .create({
-      body: 'I am a Gatsby Function.',
+      body: body.message,
+      from: '+14157022985',
+      to: '+15183387907'
+    }).then(message => 
+      res.setStatus(200).json({
+        status: 'success',
+        id:message.sid
+      }));
+  }
+
+  return client.messages
+    .create({
+      body: message,
       from: '+14157022985',
       to: '+15183387907'
     }).then(message => 
