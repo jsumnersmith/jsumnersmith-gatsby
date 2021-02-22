3175c8a2dad364aa62fc68dec2e2467f3de614cd
Joel Smith <jsumnersmith@gmail.com>
2021-02-22 13:10:53 -0500

    asdfnsad
    g

--- src/functions/sendMail.js
+++ src/functions/sendMail.js
@@ -19,6 +19,7 @@ module.exports = (req, res) => {
         error: error.response
       })
     }
+    
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
     console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
   });
