2ce4f287e2374dbbbb216c04685bfcbb03860884
Joel Smith <jsumnersmith@gmail.com>
2021-02-22 13:55:12 -0500

    updated.

--- src/functions/sendSMS.js
+++ src/functions/sendSMS.js
@@ -1,13 +1,15 @@
+const account_sid = process.env.GATSBY_TWILIO_ACCOUNT_SID;
+const auth_token = process.env.GATSBY_TWILIO_AUTH_TOKEN;
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 module.exports = (req, res) => {
-  const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+  client()
 
   client.messages
     .create({
       body: 'I am a Gatsby Function.',
       from: '+14157022985',
       to: '+15183387907'
-    })
-    .then(message => 
+    }).then(message => 
       res.setStatus(200).json({
         status: 'success',
         id:message.sid
