b79d85a764ceb02d7d4266353a1db5161ffb5e52
Joel Smith <jsumnersmith@gmail.com>
2021-02-24 16:23:29 -0500

    Stashing

--- src/functions/sendSMS.js
+++ src/functions/sendSMS.js
@@ -1,32 +1,23 @@
-// require('dotenv');
-// const account_sid = process.env.GATSBY_TWILIO_ACCOUNT_SID;
-// const auth_token = process.env.GATSBY_TWILIO_AUTH_TOKEN;
 const client = require('twilio')(process.env.GATSBY_TWILIO_ACCOUNT_SID,  process.env.GATSBY_TWILIO_AUTH_TOKEN);
 
 module.exports = (req, res) => {
   const body = req.body;
-  
-  if (body && body.message) {
-    return client.messages
-    .create({
-      body: body.message,
-      from: '+14157022985',
-      to: '+15183387907'
-    }).then(message => 
-      res.setStatus(200).json({
-        status: 'success',
-        id:message.sid
-      }));
-  }
 
+  if (!body.message){
+    res.setStatus(400).send("Message Required")
+  }
+  
+  if (!body.number){
+    res.setStatus(400).send("Number Required")
+  }
+  
   return client.messages
     .create({
-      body: "I'm a gatsby function",
-      from: '+14157022985',
-      to: '+15183387907'
-    }).then(message => 
-      res.sendStatus(200).json({
+      body: body.message,
+      from: process.env.GATSBY_TWILIO_NUMBER,
+      to: body.number
+    }).then(message => res.setStatus(200).json({
         status: 'success',
         id:message.sid
-      }));
+    }));
 }
