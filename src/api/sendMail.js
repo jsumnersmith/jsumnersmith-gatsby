e300a985c3f09107d14457a058ec3046ebf4ee2e
Joel Smith <jsumnersmith@gmail.com>
2021-03-30 09:16:44 -0400

    Trimmed up and updated package.

--- src/api/sendMail.js
+++ src/api/sendMail.js
@@ -13,7 +13,7 @@ module.exports = (req, res) => {
     res.json({message: "Try a POST!"})
   }
   if (req.method == "POST"){
-    console.log("BODY", req.body);
+    console.log("BODY", req.body); //returns undefined.
     let message = msg;
     if (req.body && req.body.msg) {
       message = req.body.msg;
@@ -23,21 +23,15 @@ module.exports = (req, res) => {
       .then(() => {
         const used = process.memoryUsage().heapUsed / 1024 / 1024;
         console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
-
         res.status(200).json({
         message: "I will send email"
       })}, error => {
         console.error(error);
-
         if (error.response) {
           return res.status(500).json({
             error: error.response
           })
         }
-        
       });
   }
-  
-  
-  
 }
